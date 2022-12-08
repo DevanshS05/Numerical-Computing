f1c153db39c8b758c14d9c7f4e4ac60abada25a6
Devansh Singh <Devansh@Devanshs-MacBook-Air.local>
2022-12-08 17:22:06 +0530

    Added Binary to Decimal + vice-versa

--- 01-Initial-Programs/binary-to-decimal.c
+++ 01-Initial-Programs/binary-to-decimal.c
@@ -1,7 +1,30 @@
-/*Program to convert a given binary number to decimal*/
-#include<stdio.h>
+//Program to convert a binary number to its decimal equivalent
+#include <stdio.h>
 
-int main(){
+int power(int base, int exp){
+    int ans=1;
+    for(int i=0;i<exp;i++) ans*=base;
+    return ans;
+}
 
+int main() {
+    char arr[20];
+    printf("Please enter the binary number: ");
+    scanf("%s", arr);
+    
+    //Calculating the position of the point
+    int i=0;
+    for(;i<strlen(arr);i++){
+        if(arr[i]=='.') break;
+    }
+
+    float num=0;
+    //Converting whole no part to decimal
+    for(int j=i-1;j>=0;j--) num+=(power(2,i-1-j)*((int)arr[j]-48));
+    
+    //Converting the fractional part to decimal
+    for(int j=i+1;j<strlen(arr);j++) num+=(1.0/power(2,j-i))*((int)arr[j]-48);
+    
+    printf("\nThe equivalent decimal number is: %f", num);
     return 0;
 }
--- 01-Initial-Programs/decimal-to-binary.c
+++ 01-Initial-Programs/decimal-to-binary.c
@@ -1,41 +1,36 @@
-/*Program to convert a given decimal number to binary*/
-#include<stdio.h>
-
-int main(){
-    long w;
-    float d;
-    printf("The decimal number is represented in the following format 1234.6534");
-    printf("\nKindly enter the whole no. and fractional part independently as asked: ");
-    printf("\nPlease enter the whole no. part: ");
-    scanf("%ld", &w);
-    printf("\nPlease enter the fractional part: ");
-    printf("%f",&d);
-
-    printf("\n");//Printing the converted number from the next line
-    //Converting the whole number part to binary
-    while(w>1){
-        printf("%ld",w%2);
-        w=w/2;
-        if(w==1){
-            printf("1");
-            break;
-        }
+#include <stdio.h>
+#include <math.h>
+int main() {
+    //Read the number as a float
+    float num;
+    printf("Please enter the decimal number you want to convert: ");
+    scanf("%f",&num);
+    long whole = (long)floor(num);
+    float dec = num-whole;
+    
+    //Converting the whole number to binary
+    long binWhole=0;
+    int i = 1;
+    while(whole>0){
+        long rem = whole%2;
+        whole = whole/2;
+        binWhole += rem*i;
+        i*=10;
     }
-    //Printing the decimal point
-    printf(".");
-    //Converting the decimal point to binary
-    for(int i=0;i<5;i++){
-        d*=2.0;
-        if(d>=1){
-            printf("1");
-            if(d==1.0){
-                break;
-            }
-            d-=1;
+    printf("\nThe converted number is: %ld.", binWhole);
+    //Converting the float part to binary
+    //Limiting the conversion to a maximum of 6 decimal places
+    if(dec==0) printf("0"); //Edge Case
+    i=0;
+    while(dec!=0 && i<6){
+        dec*=2;
+        if(dec>=1){
+            dec-=1;
+            printf("1");  
         }
-        else{
-            printf("0");
-        }
+        else printf("0");
+        i++;
     }
+    //printf("The converted whole number part is: %ld", binWhole);
     return 0;
 }
